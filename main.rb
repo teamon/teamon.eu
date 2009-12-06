cb972e34ae80c28a148836f386ff49c0a6543ea5
Tymon Tobolski <i@teamon.eu>
2009-12-06 18:07:25 +0100

    Grunwald

--- main.rb
+++ main.rb
@@ -4,12 +4,28 @@ require 'erb'
 
 require 'jarbuilder'
 require 'klp'
+require 'grunwald'
 
 set :environment, :production
 set :port, 3031
 
 get '/' do
   erb :index
+end
+
+get '/grunwald' do
+  <<-EOS
+     <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN">
+     <html>
+       <head>
+         <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+         <title></title>
+       </head>
+       <body>
+         #{Grunwald.offblast!}
+       </body>
+     </html>
+   EOS
 end
 
 get '/klp' do
